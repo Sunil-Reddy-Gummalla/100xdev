db81b95c473c2a40ec4f5db53e41e912191b2c52
Sunil-Reddy-Gummalla <gummalla.sunilreddy@gmail.com>
2023-12-03 21:06:56 +0530

    zerodha landing page

---
+++ assignment-0/zerodha-landing-page/index.js
@@ -0,0 +1,8 @@
+const returns = document.getElementById('returns');
+const amount = document.getElementById('amount');
+const time = document.getElementById('time');
+const intreast = document.getElementById('intreast');
+console.log(returns);
+console.log(amount);
+console.log(time);
+console.log(intreast);
